0cebeb16804667be2e3dc00d6cbe5525f95327fe
Matthew Costabile <mattcosta7@github.com>
2022-01-09 01:32:55 +0000

    fix action arrangement

--- src/parse-stats-file-to-json.ts
+++ src/parse-stats-file-to-json.ts
@@ -5,15 +5,11 @@ import {resolve} from 'path'
 export async function parseStatsFileToJson(
   statsFilePath: string
 ): Promise<Pick<StatsCompilation, 'assets'>> {
-  try {
     console.log({cwd: process.cwd()})
     const path = resolve(process.cwd(), statsFilePath)
-    console.log({path})
     const file = await readFile(path, 'utf8')
-    console.log(file)
     return JSON.parse(file) as StatsCompilation
   } catch (e) {
-    console.log(e)
     return {assets: []} as Pick<StatsCompilation, 'assets'>
   }
 }
